d7f33af384f3b9d10a55b2d92de016d4728e8cd1
AuroSampad2003 <aurosampadchampatiray573@gmail.com>
2025-02-16 16:47:57 +0530

    secondary commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import MatrixTransform from "./components/MatrixTransform";
-import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <MatrixTransform />
-      </div>
-    </Router>
+    <div className="App">
+      <MatrixTransform />
+    </div>
   );
 }
 
